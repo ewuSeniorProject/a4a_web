f9ee17a30e481e907f94ed7de7186eac7d0a57a1
mizesolutions <brian@mizesolutions.com>
2018-03-12 09:25:37 -0700

    Removed support structure for removed illuminate database Restored original dtabase container information

--- a4aa/src/dependencies.php
+++ a4aa/src/dependencies.php
@@ -18,14 +18,14 @@ $container['logger'] = function ($c) {
     return $logger;
 };
 
-//// PDO database library
-//$container['db'] = function ($c) {
-//    $settings = $c->get('settings')['db'];
-//    $pdo = new PDO("mysql:host=" . $settings['host'] . ";dbname=" . $settings['dbname'], $settings['user'], $settings['pass']);
-//    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-//    return $pdo;
-//};
+// PDO database library
+$container['db'] = function ($c) {
+    $settings = $c->get('settings')['db'];
+    $pdo = new PDO("mysql:host=" . $settings['host'] . ";dbname=" . $settings['dbname'], $settings['user'], $settings['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
 
 // Register component on container
 $container['view'] = function ($container) {
