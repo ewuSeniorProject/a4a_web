49f0ac0ea2e8a58da572645fb20a8bd8136d6896
mizesolutions <brian@mizesolutions.com>
2018-03-21 09:56:19 -0700

    Updating front end

--- a4aa/public/script/update.js
+++ a4aa/public/script/update.js
@@ -3,10 +3,107 @@ const SESSIONID = getSessionID();
 console.log("est_id: " + SESSIONID);
 
 $(document).ready(function () {
+
+    function EstablishmentModel(parm) {
+        console.log(JSON.stringify(parm));
+        this.postUri = parm.postUri;
+        this.putUri = parm.putUri;
+        this.est_id = ok.observable(parm.data.est_id);
+        this.name = ko.observable(parm.data.name);
+        this.name.focused = ko.observable(false);
+        this.website = ko.observable(parm.data.website);
+        this.website.focused = ko.observable(false);
+        this.cat_id = ko.observable(parm.data.cat_id);
+        this.cat_id.focused = ko.observable(false);
+        this.subtype = ko.observable(parm.data.subtype);
+        this.subtype.focused = ko.observable(false);
+        this.config_id = ko.observable(parm.data.config_id);
+        this.config_id.focused = ko.observable(false);
+        this.street = ko.observable(parm.data.street);
+        this.street.focused = ko.observable(false);
+        this.city = ko.observable(parm.data.city);
+        this.city.focused = ko.observable(false);
+        this.state = ko.observable(parm.data.state);
+        this.state.focused = ko.observable(false);
+        this.zip = ko.observable(parm.data.zip);
+        this.zip.focused = ko.observable(false);
+        this.phone = ko.observable(parm.data.phone);
+        this.phone.focused = ko.observable(false);
+        this.phone_type = ko.observable(parm.data.phone_type);
+        this.phone_type.focused = ko.observable(false);
+        this.contact_fname = ko.observable(parm.data.contact_fname);
+        this.contact_fname.focused = ko.observable(false);
+        this.contact_lname = ko.observable(parm.data.contact_lname);
+        this.contact_lname.focused = ko.observable(false);
+        this.contact_title = ko.observable(parm.data.contact_title);
+        this.contact_title.focused = ko.observable(false);
+        this.contact_email = ko.observable(parm.data.contact_email);
+        this.contact_email.focused = ko.observable(false);
+        this.date = ko.observable(parm.data.date);
+        this.date.focused = ko.observable(false);
+        this.user_id = ko.observable(parm.data.user_id);
+        this.user_id.focused = ko.observable(false);
+    }
+
+    function EstablishmentViewModel(getUri, deleteUri, postUri, putUri) {
+        var self = this;
+        self.establishmentList = ko.observableArray([]);
+
+        $.getJSON(getUri, function (data) {
+            self.establishmentList($.map(data, function (item) {
+                return new EstablishmentModel({data:item, postUri:postUri, putUri:putUri});
+            }));
+        });
+
+        self.removeItem = function (item) {
+            var con = confirm("Delete this record?");
+            if (con){
+                self.establishmentList.remove(item);
+                removeRequest(deleteUri, item.est_id());
+            }
+        };
+
+        self.addItem = function () {
+            self.establishmentList.push(new EstablishmentModel({name: "", postUri:postUri, putUri:putUri}));
+        };
+    }
+
+    function CategoryModel(parm) {
+        console.log(JSON.stringify(parm));
+        this.postUri = parm.postUri;
+        this.putUri = parm.putUri;
+        this.category_id = ko.observable(parm.data.cat_id);
+        this.category_id.focused = ko.observable(false);
+        this.name = ko.observable(parm.data.name);
+        this.name.focused = ko.observable(false);
+    }
+
+    function CategoryViewModel(getUri, deleteUri, postUri, putUri) {
+        var self = this;
+        self.categoryList = ko.observableArray([]);
+
+        $.getJSON(getUri, function (data) {
+            self.categoryList($.map(data, function (item) {
+                return new CategoryModel({data:item, postUri:postUri, putUri:putUri});
+            }));
+        });
+
+        // self.removeItem = function (item) {
+        //     var con = confirm("Delete this record?");
+        //     if (con){
+        //         self.obsList.remove(item);
+        //         removeRequest(deleteUri, item.name());
+        //     }
+        // };
+        //
+        // self.addItem = function () {
+        //     self.obsList.push(new Model({name: "", postUri:postUri, putUri:putUri}));
+        // };
+    }
+
+
     ko.applyBindings(new EstablishmentViewModel(API_ROOT + 'get/establishment/' + SESSIONID, API_ROOT + 'delete/establishment/' + SESSIONID, API_ROOT + 'post/establishment/' + SESSIONID, API_ROOT + 'put/establishment/' + SESSIONID), document.getElementById('establishment-view'));
-    ko.cleanNode($element[0]);
     ko.applyBindings(new CategoryViewModel(API_ROOT + '/category/', API_ROOT + 'delete/category/', API_ROOT + 'post/category/', API_ROOT + 'put/category/'), document.getElementById('category-view'));
-    ko.cleanNode($element[0]);
     // ko.applyBindings(new ViewModel(API_ROOT + 'get/components', API_ROOT + 'delete/component', API_ROOT + 'post/component', API_ROOT + 'put/component'), document.getElementById('components-view'));
     // ko.applyBindings(new ViewModel(API_ROOT + 'get/durations', API_ROOT + 'delete/duration', API_ROOT + 'post/duration', API_ROOT + 'put/duration'), document.getElementById('durations-view'));
     // ko.applyBindings(new ViewModel(API_ROOT + 'get/ranges', API_ROOT + 'delete/range', API_ROOT + 'post/range', API_ROOT + 'put/range'), document.getElementById('ranges-view'));
@@ -62,99 +159,3 @@ function newRecordRequest(uri, record){
     });
 }
 
-function EstablishmentModel(parm) {
-    console.log(JSON.stringify(parm));
-    this.postUri = parm.postUri;
-    this.putUri = parm.putUri;
-    this.est_id = ok.observable(parm.data.est_id);
-    this.name = ko.observable(parm.data.name);
-    this.name.focused = ko.observable(false);
-    this.website = ko.observable(parm.data.website);
-    this.website.focused = ko.observable(false);
-    this.cat_id = ko.observable(parm.data.cat_id);
-    this.cat_id.focused = ko.observable(false);
-    this.subtype = ko.observable(parm.data.subtype);
-    this.subtype.focused = ko.observable(false);
-    this.config_id = ko.observable(parm.data.config_id);
-    this.config_id.focused = ko.observable(false);
-    this.street = ko.observable(parm.data.street);
-    this.street.focused = ko.observable(false);
-    this.city = ko.observable(parm.data.city);
-    this.city.focused = ko.observable(false);
-    this.state = ko.observable(parm.data.state);
-    this.state.focused = ko.observable(false);
-    this.zip = ko.observable(parm.data.zip);
-    this.zip.focused = ko.observable(false);
-    this.phone = ko.observable(parm.data.phone);
-    this.phone.focused = ko.observable(false);
-    this.phone_type = ko.observable(parm.data.phone_type);
-    this.phone_type.focused = ko.observable(false);
-    this.contact_fname = ko.observable(parm.data.contact_fname);
-    this.contact_fname.focused = ko.observable(false);
-    this.contact_lname = ko.observable(parm.data.contact_lname);
-    this.contact_lname.focused = ko.observable(false);
-    this.contact_title = ko.observable(parm.data.contact_title);
-    this.contact_title.focused = ko.observable(false);
-    this.contact_email = ko.observable(parm.data.contact_email);
-    this.contact_email.focused = ko.observable(false);
-    this.date = ko.observable(parm.data.date);
-    this.date.focused = ko.observable(false);
-    this.user_id = ko.observable(parm.data.user_id);
-    this.user_id.focused = ko.observable(false);
-}
-
-function EstablishmentViewModel(getUri, deleteUri, postUri, putUri) {
-    var self = this;
-    self.establishmentList = ko.observableArray([]);
-
-    $.getJSON(getUri, function (data) {
-        self.establishmentList($.map(data, function (item) {
-            return new EstablishmentModel({data:item, postUri:postUri, putUri:putUri});
-        }));
-    });
-
-    self.removeItem = function (item) {
-        var con = confirm("Delete this record?");
-        if (con){
-            self.establishmentList.remove(item);
-            removeRequest(deleteUri, item.est_id());
-        }
-    };
-
-    self.addItem = function () {
-        self.establishmentList.push(new EstablishmentModel({name: "", postUri:postUri, putUri:putUri}));
-    };
-}
-
-function CategoryModel(parm) {
-    console.log(JSON.stringify(parm));
-    this.postUri = parm.postUri;
-    this.putUri = parm.putUri;
-    this.category_id = ko.observable(parm.data.cat_id);
-    this.category_id.focused = ko.observable(false);
-    this.name = ko.observable(parm.data.name);
-    this.name.focused = ko.observable(false);
-}
-
-function CategoryViewModel(getUri, deleteUri, postUri, putUri) {
-    var self = this;
-    self.categoryList = ko.observableArray([]);
-
-    $.getJSON(getUri, function (data) {
-        self.categoryList($.map(data, function (item) {
-            return new CategoryModel({data:item, postUri:postUri, putUri:putUri});
-        }));
-    });
-
-    // self.removeItem = function (item) {
-    //     var con = confirm("Delete this record?");
-    //     if (con){
-    //         self.obsList.remove(item);
-    //         removeRequest(deleteUri, item.name());
-    //     }
-    // };
-    //
-    // self.addItem = function () {
-    //     self.obsList.push(new Model({name: "", postUri:postUri, putUri:putUri}));
-    // };
-}
