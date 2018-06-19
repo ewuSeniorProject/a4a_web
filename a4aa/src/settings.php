76c8c91fd95d4c1bde31724241b2cdd8c16b971c
Brian Mize <brian@mizesolutions.com>
2018-06-19 16:16:30 -0700

    added settings.php

---
+++ a4aa/src/settings.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+
+        // Database connection settings
+        'db' => [
+            'host' => '66.147.244.98',
+            'dbname' => 'mizesolu_a4aa',
+            'user' => 'mizesolu_a4aaB',
+            'pass' => 'QxUP=r@JE10P'
+        ],
+
+    ],
+];
