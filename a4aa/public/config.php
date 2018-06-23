6574d76dce5495a8737b391272988b8c75081f3d
Brian Mize <brian.r.mize@gmail.com>
2018-06-23 10:04:01 -0700

    Update config.php

--- a4aa/public/config.php
+++ a4aa/public/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '66.147.244.98');
-define('DB_USERNAME', 'mizesolu_a4aaB');
-define('DB_PASSWORD', 'QxUP=r@JE10P');
-define('DB_NAME', 'mizesolu_a4aa');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'a4as_db');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
